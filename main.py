84e67b60b1a51ceed8c892b1da68c615c8592329
onnes <onnes1108@gmail.com>
2023-04-18 20:08:04 +0800

    Basic functionality implementation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import math
 import struct
 import numpy as np
@@ -31,11 +32,11 @@ def d_softmax(data):
 #     value_1=softmax(test_input)
 #     test_input[i] += h
 #     value_2 = softmax(test_input)
-#     # print((value_2 - value_1) / h) # 求导公式 
+#     # print((value_2 - value_1) / h) # 求导公式
 #     # print(dervative[i]) # 推出来的公式
 #     print(dervative[i] - ((value_2 - value_1) / h))
 
-# # 验证 d_tanh 
+# # 验证 d_tanh
 # h = 0.00001
 # input_len = 4
 # for i in range(input_len):
@@ -44,7 +45,7 @@ def d_softmax(data):
 #     value_1 = tanh(test_input)
 #     test_input[i] += h
 #     value_2 = tanh(test_input)
-#     # print((value_2 - value_1) / h) # 求导公式 
+#     # print((value_2 - value_1) / h) # 求导公式
 #     # print(dervative[i]) # 推出来的公式
 #     print(dervative[i] - ((value_2 - value_1) / h))
 
@@ -93,7 +94,7 @@ def sqr_loss(img, lab, parameters):
 def grad_parameters(img, lab, parameters):
     l_0_in = img + parameters[0]['b']
     l_0_out = activation[0](l_0_in)
-    l_1_in = np.dot(l_0_out, parameters[1]['w'] + parameters[1]['b'])
+    l_1_in = np.dot(l_0_out, parameters[1]['w']) + parameters[1]['b']
     l_1_out = activation[1](l_1_in)
 
     diff = one_hot[lab] - l_1_out;
@@ -105,7 +106,6 @@ def grad_parameters(img, lab, parameters):
     grad_w_1 = -2 * np.outer(l_0_out, act_1)
     # print(l_0_out)
     return {'b0': grad_b_0, 'b1': grad_b_1, 'w1': grad_w_1}
-
 
 def show_train(index):
     print("lab:", end = ' '), print(train_lab[index])
@@ -122,17 +122,50 @@ def show_valid(index):
     plt.imshow(valid_img[index].reshape(28, 28), cmap='gray')
     plt.show()
 
+# 验证测试集
+def valid_loss(parameters):
+    loss_accu = 0 # 偏差值的累加
+    for img_i in range(valid_num): # 测试集的数量 10000
+        loss_accu += sqr_loss(valid_img[img_i], valid_lab[img_i], parameters)
+    return loss_accu
+
+# valid 集合的验证 求 valid 集合的准确率
+def valid_accuracy(parameters):
+    correct = [predict(valid_img[img_i], parameters).argmax() == valid_lab[img_i] for img_i in range(valid_num)]
+    # print("validation accuracy: {}".format(correct.count(True) / len(correct)))
+    return correct.count(True) / len(correct)
+
+# 分组训练减少时间
+def train_batch(current_batch, parameters):
+    # 集合梯度初始化
+    grad_accu = grad_parameters(train_img[current_batch * batch_size + 0], train_lab[current_batch * batch_size + 0], parameters)
+    for img_i in range(1, batch_size):
+        grad_tmp = grad_parameters(train_img[current_batch*batch_size + img_i], train_lab[current_batch * batch_size + img_i], parameters)
+        for key in grad_accu.keys():
+            grad_accu[key] += grad_tmp[key] # += b0 b1 w1 ......
+    for key in grad_accu.keys(): # 做平均
+        grad_accu[key] /= batch_size
+    return grad_accu
+
+# 梯度下降
+def combine_parameters(parameters, grad, learn_rate):
+    parameter_tmp = copy.deepcopy(parameters)
+    parameter_tmp[0]['b'] -= learn_rate * grad['b0']
+    parameter_tmp[1]['b'] -= learn_rate * grad['b1']
+    parameter_tmp[1]['w'] -= learn_rate * grad['w1']
+    return parameter_tmp
+
 dimensions = [28 * 28, 10] # 存储两个数据 输入数量 m 和 输出数量 n
 activation = [tanh, softmax]
 
 distribution = [ # 列表套字典
-    {'b' : [0, 0]}, # 第一层的偏置函数和第二层的偏置函数推荐设置为 1
+    {'b' : [0, 0]}, # 第一层的偏置函数和第二层的偏置函数推荐设置为 0
     {'b' : [0, 0], 'w': [-math.sqrt(6 / (dimensions[0] + dimensions[1])), math.sqrt(6 / (dimensions[0] + dimensions[1]))]}, # 假设只有两层 784 -> 10
 ]
 
 # 初始化
 parameters = init_paraments()
-
+differential = {softmax:d_softmax,tanh:d_tanh}
 # 测试正确性
 # print(predict(np.random.rand(784), parameters))
 
@@ -215,3 +248,19 @@ one_hot = np.identity(dimensions[-1]) # 10 * 10 的单位矩阵(对角线都是 
 #     value2=sqr_loss(train_img[img_i],train_lab[img_i],test_parameters)
 #     grad_list.append(derivative[i] - (value2 - value1) / h)
 # print(np.abs(grad_list).max())
+# print(valid_loss(parameters))
+# print(valid_accuracy(parameters))
+
+batch_size = 100 # 100 组
+# print(train_batch(0, parameters)['w1'])
+# print(parameters)
+# print(combine_parameters(parameters, train_batch(0, parameters), 1))
+
+print(valid_accuracy(parameters)) # 之前的正确率
+# 训练
+for i in range(train_num // batch_size):
+    if (i % 100 == 99):
+        print("runing batch {} / {}".format(i + 1, train_num // batch_size))
+    grad_tmp = train_batch(i, parameters)
+    parameters = combine_parameters(parameters, grad_tmp, 1)
+print(valid_accuracy(parameters)) # 训练之后的正确率
